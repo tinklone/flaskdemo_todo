3154446a344dbd38931d63133fd82fa502999575
tinklone <lonetan@gmail.com>
2017-09-26 00:53:48 +0800

    vscode git plugin test

--- run.py
+++ run.py
@@ -17,6 +17,7 @@ for index in range(len(modules)):
     view_class = getattr(import_module,view_classes[index])
     cls_obj = view_class()
     cls_obj.register(app)
+print ""
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
