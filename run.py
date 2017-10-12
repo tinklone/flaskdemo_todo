3c3bd9ed8e55c5cecfde100a7e7f75432b4261c1
xiaoqi7s <569861129@qq.com>
2017-10-12 19:14:47 +0800

    change
    Signed-off-by: xiaoqi7s <569861129@qq.com>

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
-#coding:utf-8
+# coding:utf-8
 from app import app
 import importlib
-from library.ViewsHandler import  get_views,get_class
+from library.ViewsHandler import get_views, get_class
 
 from views.QuotesView import QuotesView
 from views.TestView import TestView
-
-
 
 modules = get_views()
 view_classes = get_class()
@@ -14,9 +12,12 @@ print modules
 for index in range(len(modules)):
     import_module = importlib.import_module(modules[index])
     print(dir(import_module))
-    view_class = getattr(import_module,view_classes[index])
+    view_class = getattr(import_module, view_classes[index])
     cls_obj = view_class()
-    cls_obj.register(app)
+    if view_classes[index] == 'IndexView':
+        cls_obj.register(app, route_base='/')
+    else:
+        cls_obj.register(app)
 print "vscode test"
 
 if __name__ == '__main__':
